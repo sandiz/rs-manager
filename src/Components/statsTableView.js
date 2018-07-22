37609dbd10871ab2ab3cc8edd4055a28cf1423b6
Sandi Chakravarty <sandi@fortmasongames.com>
2018-07-21 23:52:01 -0700

    dashboard view with stats

--- src/Components/statsTableView.js
+++ src/Components/statsTableView.js
@@ -14,58 +14,60 @@ export default class StatsTableView extends React.Component {
           <tr>
             <td>Total</td>
             <td className="">
-              <span id="lead_total" className="ta-right right-align header-top">{this.props.total}</span>
+              <span id="lead_total" className="ta-right right-align header-top">
+                {this.props.total}
+              </span>
             </td>
           </tr>
           <tr>
-            <td>95-100%</td>
+            <td className={this.props.scoreattack ? "iconPreview gp_platinum badgeText dashboardGP" : ""}>{!this.props.scoreattack ? "95-100%" : "Plat"}</td>
             <td className="ta-left skinny">
               <span>
                 <svg id="lead_tier_1_svg" height="100%" width="100%">
-                  <rect width={this.props.highscorewidth + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                  <rect width={(this.props.scoreattack ? this.props.platwidth : this.props.highscorewidth) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
                 </svg>
               </span>
               <span className="ta-right right-align">
-                {this.props.highscoretotal}
+                {this.props.scoreattack ? this.props.plattotal : this.props.highscoretotal}
               </span>
             </td>
           </tr>
           <tr>
-            <td>90-95%</td>
+            <td className={this.props.scoreattack ? "iconPreview gp_gold badgeText dashboardGP" : ""}>{!this.props.scoreattack ? "90-95%" : "Gold"}</td>
             <td className="ta-left skinny">
               <span>
                 <svg id="lead_tier_1_svg" height="100%" width="100%">
-                  <rect width={this.props.mediumscorewidth + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                  <rect width={(this.props.scoreattack ? this.props.goldwidth : this.props.mediumscorewidth) + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
                 </svg>
                 <span className="ta-right right-align">
-                  {this.props.mediumscoretotal}
+                  {this.props.scoreattack ? this.props.goldtotal : this.props.mediumscoretotal}
                 </span>
               </span>
             </td>
           </tr>
           <tr>
-            <td>1-90%</td>
+            <td className={this.props.scoreattack ? "iconPreview gp_silver badgeText dashboardGP" : ""}>{!this.props.scoreattack ? "1-90%" : "Silv"}</td>
             <td className="ta-left skinny">
               <span>
                 <svg id="lead_tier_1_svg" height="100%" width="100%">
-                  <rect width={this.props.lowscorewidth + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                  <rect width={(this.props.scoreattack ? this.props.silverwidth : this.props.lowscorewidth) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
                 </svg>
               </span>
               <span className="ta-right right-align">
-                {this.props.lowscoretotal}
+                {this.props.scoreattack ? this.props.silvertotal : this.props.lowscoretotal}
               </span>
             </td>
           </tr>
           <tr>
-            <td>Unplayed</td>
+            <td className={this.props.scoreattack ? "iconPreview gp_bronze badgeText dashboardGP" : ""}>{!this.props.scoreattack ? "Unplayed" : <span className="badgeTextSmall">Brnz</span>}</td>
             <td className="ta-left skinny">
               <span>
                 <svg id="lead_tier_1_svg" height="100%" width="100%">
-                  <rect width={this.props.unplayedwidth + "%"} height="100%" style={{ fill: 'lightgray', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                  <rect width={(this.props.scoreattack ? this.props.bronzewidth : this.props.unplayedwidth) + "%"} height="100%" style={{ fill: 'lightgray', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
                 </svg>
               </span>
               <span className="ta-right right-align">
-                {this.props.unplayedtotal}
+                {this.props.scoreattack ? this.props.bronzetotal : this.props.unplayedtotal}
               </span>
             </td>
           </tr>
@@ -84,6 +86,15 @@ StatsTableView.propTypes = {
   lowscorewidth: PropTypes.number,
   unplayedtotal: PropTypes.number,
   unplayedwidth: PropTypes.number,
+  scoreattack: PropTypes.bool,
+  plattotal: PropTypes.number,
+  platwidth: PropTypes.number,
+  goldtotal: PropTypes.number,
+  goldwidth: PropTypes.number,
+  silvertotal: PropTypes.number,
+  silverwidth: PropTypes.number,
+  bronzetotal: PropTypes.number,
+  bronzewidth: PropTypes.number,
 }
 StatsTableView.defaultProps = {
   total: 0,
@@ -95,4 +106,13 @@ StatsTableView.defaultProps = {
   lowscorewidth: 0,
   unplayedtotal: 0,
   unplayedwidth: 0,
+  scoreattack: false,
+  plattotal: 0,
+  platwidth: 0,
+  goldtotal: 0,
+  goldwidth: 0,
+  silvertotal: 0,
+  silverwidth: 0,
+  bronzetotal: 0,
+  bronzewidth: 0,
 }
