60ee2800d2b933b5d29c00b76f545c4b16b31fac
Sandi Chakravarty <sandi.newton@gmail.com>
2018-09-08 23:51:30 -0700

    convert bash script to js

---
+++ scripts/package.js
@@ -0,0 +1,53 @@
+const spawn = require('cross-spawn');
+
+if(process.argv.length <= 2)
+{
+    console.log("Need platform (win/mac)");
+    process.exit(-1);
+}
+const buildPlatform = process.argv[2];
+
+let platform = ""
+let icon = ""
+let extra_ignore = ""
+if (buildPlatform === "win") {
+    platform = "win32";
+    icon = "src/assets/icons/win/rs.ico"
+    extra_ignore = ""
+}
+else if (buildPlatform === "mac") {
+    platform = "darwin"
+    icon = "src/assets/icons/mac/mac.icns"
+    extra_ignore = "src/tools/RockSniffer"
+}
+else {
+    console.log("Invalid buildPlatform, valid options: mac, win");
+    process.exit(-1);
+}
+
+
+const child = spawn("yarn run electron-packager", [
+    ".",
+    "Rocksmith Manager",
+    "--out=release-builds",
+    "--overwrite",
+    "--prune=true",
+    "--arch=x64",
+    `--platform=${platform}`,
+    `--icon=${icon}`,
+    "--ignore=config.dev.json",
+    "--ignore=rsdb.dev.sqlite",
+    "--ignore=assets/",
+    extra_ignore.length > 0 ? `--ignore=${extra_ignore}` : ""
+  ]);
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  child.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
