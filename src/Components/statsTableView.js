0d5d937bdb999f9bf1934c67288223ee481df056
Sandi Chakravarty <sandi.newton@gmail.com>
2018-09-28 22:14:22 -0700

    add more tiers to lead/rhythm/bass stats

--- src/Components/statsTableView.js
+++ src/Components/statsTableView.js
@@ -8,146 +8,274 @@ export default class StatsTableView extends React.Component {
   render = () => {
     return (
       <table style={{ width: 100 + '%', marginTop: 7 + 'px' }} >
-        <tbody>
-          <tr>
-            <td>Total</td>
-            <td className="">
-              <span id="lead_total" className="ta-right right-align header-top">
-                {this.props.total}
-              </span>
-            </td>
-          </tr>
-          {
-            this.props.scoreattack ?
-              <tr>
-                <td
-                  className="iconPreview gp_fcs badgeText dashboardGP"
-                  title="Total Full Combos">
-                  FCs
+        {
+          this.props.scoreattack ?
+            (
+              <tbody>
+                <tr>
+                  <td>Total</td>
+                  <td className="">
+                    <span id="lead_total" className="ta-right right-align header-top">
+                      {this.props.total}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_fcs badgeText dashboardGP"
+                    title="Total Full Combos">
+                    FCs
                 </td>
-                <td className="ta-left skinny">
-                  <span>
-                    <svg id="fcs_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                      <rect width={(this.props.tierWidths[6]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                    </svg>
-                  </span>
-                  <span className="ta-right right-align">
-                    {this.props.tierTotals[6]}
-                  </span>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="fcs_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[6]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[6]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_platinum badgeText dashboardGP"
+                    title="Total Platinum Badges">
+                    Plat
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[0]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[0]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_gold badgeText dashboardGP"
+                    title="Total Gold Badges"
+                  >
+                    Gold
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[1]) + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                      <span className="ta-right right-align">
+                        {this.props.tierTotals[1]}
+                      </span>
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_silver badgeText dashboardGP"
+                    title="Total Silver Badges"
+                  >
+                    Silv
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[2]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[2]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_bronze badgeText  GP"
+                    title="Total Bronze Badges">
+                    Brnz
                 </td>
-              </tr>
-              : null
-          }
-          <tr>
-            <td
-              className={this.props.scoreattack ? "iconPreview gp_platinum badgeText dashboardGP" : ""}
-              title={this.props.scoreattack ? "Total Platinum Badges" : "Mastery between 95-100%"}>
-              {!this.props.scoreattack ? "95-100%" : "Plat"}
-            </td>
-            <td className="ta-left skinny">
-              <span>
-                <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                  <rect width={(this.props.scoreattack ? this.props.tierWidths[0] : this.props.masteryWidths[0]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                </svg>
-              </span>
-              <span className="ta-right right-align">
-                {this.props.scoreattack ? this.props.tierTotals[0] : this.props.masteryTotals[0]}
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td
-              className={this.props.scoreattack ? "iconPreview gp_gold badgeText dashboardGP" : ""}
-              title={this.props.scoreattack ? "Total Gold Badges" : "Mastery between 90-95%"}
-            >
-              {!this.props.scoreattack ? "90-95%" : "Gold"}
-            </td>
-            <td className="ta-left skinny">
-              <span>
-                <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                  <rect width={(this.props.scoreattack ? this.props.tierWidths[1] : this.props.masteryWidths[1]) + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                </svg>
-                <span className="ta-right right-align">
-                  {this.props.scoreattack ? this.props.tierTotals[1] : this.props.masteryTotals[1]}
-                </span>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td
-              className={this.props.scoreattack ? "iconPreview gp_silver badgeText dashboardGP" : ""}
-              title={this.props.scoreattack ? "Total Silver Badges" : "Mastery between 1-90%"}
-            >
-              {!this.props.scoreattack ? "1-90%" : "Silv"}
-            </td>
-            <td className="ta-left skinny">
-              <span>
-                <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                  <rect width={(this.props.scoreattack ? this.props.tierWidths[2] : this.props.masteryWidths[2]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                </svg>
-              </span>
-              <span className="ta-right right-align">
-                {this.props.scoreattack ? this.props.tierTotals[2] : this.props.masteryTotals[2]}
-              </span>
-            </td>
-          </tr>
-          {
-            this.props.scoreattack ?
-              <tr>
-                <td
-                  className={this.props.scoreattack ? "iconPreview gp_bronze badgeText  GP" : "hidden"}
-                  title={this.props.scoreattack ? "Total Bronze Badges" : ""}>
-                  Brnz
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[3]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[3]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="iconPreview gp_failed badgeText dashboardGP"
+                    title="Total Failed">
+                    Fail
                 </td>
-                <td className="ta-left skinny">
-                  <span>
-                    <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                      <rect width={(this.props.tierWidths[3]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                    </svg>
-                  </span>
-                  <span className="ta-right right-align">
-                    {this.props.tierTotals[3]}
-                  </span>
-                </td>
-              </tr> : null
-          }
-          {
-            this.props.scoreattack ?
-              <tr>
-                <td
-                  className={this.props.scoreattack ? "iconPreview gp_failed badgeText dashboardGP" : "hidden"}
-                  title={this.props.scoreattack ? "Total Failed" : ""}>
-                  Fail
-                </td>
-                <td className="ta-left skinny">
-                  <span>
-                    <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                      <rect width={(this.props.tierWidths[4]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                    </svg>
-                  </span>
-                  <span className="ta-right right-align">
-                    {this.props.tierTotals[4]}
-                  </span>
-                </td>
-              </tr> : null
-          }
-          <tr>
-            <td
-              title="Unplayed Arrangments">
-              Unplayed
-            </td>
-            <td className="ta-left skinny">
-              <span>
-                <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
-                  <rect width={(this.props.scoreattack ? this.props.tierWidths[5] : this.props.masteryWidths[3]) + "%"} height="100%" style={{ fill: 'lightgray', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
-                </svg>
-              </span>
-              <span className="ta-right right-align">
-                {this.props.scoreattack ? this.props.tierTotals[5] : this.props.masteryTotals[3]}
-              </span>
-            </td>
-          </tr>
-        </tbody>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[4]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[4]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    title="Unplayed Arrangments">
+                    Unplayed
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.tierWidths[5]) + "%"} height="100%" style={{ fill: 'lightgray', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.tierTotals[5]}
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            )
+            :
+            (
+              <tbody>
+                <tr>
+                  <td>Total</td>
+                  <td className="">
+                    <span id="lead_total" className="ta-right right-align header-top">
+                      {this.props.total}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Master Mode">
+                    100%+
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[0]) + "%"} height="100%" style={{ fill: '#26956A', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[0]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Mastery between 95-100%">
+                    95-99%
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[1]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[1]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Mastery between 90-95%">
+                    90-95%
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[2]) + "%"} height="100%" style={{ fill: 'lightgreen', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[2]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Mastery between 80-90%">
+                    80-90%
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[3]) + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[3]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Mastery between 70-80%"
+                  >
+                    70-80%
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[4]) + "%"} height="100%" style={{ fill: '#C8F749', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                      <span className="ta-right right-align">
+                        {this.props.masteryTotals[4]}
+                      </span>
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className=""
+                    title="Mastery between 0-70%"
+                  >
+                    1-70%
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[5]) + "%"} height="100%" style={{ fill: 'yellow', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[5]}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    title="Unplayed Arrangments">
+                    Unplayed
+                  </td>
+                  <td className="ta-left skinny">
+                    <span>
+                      <svg id="lead_tier_1_svg" height="100%" width="100%" className="dashboardsvg">
+                        <rect width={(this.props.masteryWidths[6]) + "%"} height="100%" style={{ fill: 'lightgray', strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+                      </svg>
+                    </span>
+                    <span className="ta-right right-align">
+                      {this.props.masteryTotals[6]}
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            )
+        }
       </table>
     );
   }
