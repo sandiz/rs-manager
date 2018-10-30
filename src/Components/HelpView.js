a9ce257032c6c5325176e0feec37cd2c1a21dc3a
Sandi Chakravarty <sandi.newton@gmail.com>
2018-10-29 23:45:11 -0700

    hookup helpview to respective views

--- src/Components/HelpView.js
+++ src/Components/HelpView.js
@@ -15,6 +15,7 @@ export default class HelpView extends React.Component {
     super(props)
     this.state = {
       fileData: '',
+      defaultReadme: 'dashboard',
     }
     this.tabname = "tab-help"
     const renderer = new marked.Renderer();
@@ -38,6 +39,12 @@ export default class HelpView extends React.Component {
 
   componentWillMount = async () => {
     this.changeTo(this.props.defaultReadme)
+  }
+
+  shouldComponentUpdate = async (nextprops, nextstate) => {
+    if (nextprops === this.props) return false;
+    this.changeTo(nextprops.defaultReadme)
+    return true
   }
 
   changeTo = async (id) => {
@@ -76,50 +83,121 @@ export default class HelpView extends React.Component {
         break;
     }
     string = new TextDecoder("utf-8").decode(data);
-    this.setState({ fileData: string });
+    this.setState({ fileData: string, defaultReadme: id });
   }
 
   render = () => {
-    if (this.props.currentTab === null) {
-      return null;
-    }
-    else if (this.props.currentTab.id === this.tabname) {
-      return (
-        <div className="container-fluid">
-          <div className="row justify-content-lg-center">
-            <div className="col col-lg-10 settings ta-center">
-              <br />
-              <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('dashboard')}>Dashboard</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('songs-owned')}>Songs Owned</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('song-details')}>Song Details</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('dlc-catalog')}>DLC Catalog</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('setlists')}>Setlists</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('setlist-option')}>Setlist Options</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('psarc-explorer')}>psarc Explorer</a>&nbsp;
-              | <a style={{ color: 'blue' }} href="#" onClick={() => this.changeTo('rs-live')}>Rocksmith Live</a>&nbsp;
-              <br /><br />
-              <div
-                id="markdown"
-                className=" settings"
-                style={{ width: 100 + '%', textAlign: 'left' }}
-                //eslint-disable-next-line
-                dangerouslySetInnerHTML={{
-                  __html:
-                    marked(this.state.fileData),
-                }} />
-            </div>
+    if (!this.props.showHelp) return null
+    const bgColor = this.props.popupMode ? "azure" : ""
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-lg-center">
+          <div className="col col-lg-10 settings ta-center" style={{ backgroundColor: bgColor }}>
+            {
+              (
+                <div>
+                  <br />
+                  <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "dashboard" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "dashboard" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('dashboard')}>Dashboard</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "songs-owned" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "songs-owned" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('songs-owned')}>Songs Owned</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "song-details" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "song-details" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('song-details')}>Song Details</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "dlc-catalog" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "dlc-catalog" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('dlc-catalog')}>DLC Catalog</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "setlists" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "setlists" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('setlists')}>Setlists</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "setlist-option" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "setlist-option" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('setlist-option')}>Setlist Options</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "psarc-explorer" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "psarc-explorer" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('psarc-explorer')}>psarc Explorer</a>&nbsp;
+                    | <a
+                    style={{
+                      color: 'blue',
+                      fontWeight: this.state.defaultReadme === "rs-live" ? "bolder" : "normal",
+                      borderBottom: this.state.defaultReadme === "rs-live" ? "1px solid" : "none",
+                    }}
+                    href="#"
+                    onClick={() => this.changeTo('rs-live')}>Rocksmith Live</a>&nbsp;
+                    | {
+                    this.props.popupMode
+                      ? (
+                        <a style={{ color: 'blue' }} href="#" onClick={this.props.closeHelp}>Close Help</a>
+                      ) : null
+                  }
+                </div>
+              )
+            }
+
+            <br /><br />
+            <div
+              id="markdown"
+              className=" settings"
+              style={{ width: 100 + '%', textAlign: 'left', backgroundColor: bgColor }}
+              //eslint-disable-next-line
+              dangerouslySetInnerHTML={{
+                __html:
+                  marked(this.state.fileData),
+              }} />
           </div>
         </div>
-      )
-    }
-    return null;
+      </div>
+    )
   }
 }
 HelpView.propTypes = {
-  currentTab: PropTypes.object,
+  //currentTab: PropTypes.object,
   defaultReadme: PropTypes.string,
+  popupMode: PropTypes.bool,
+  showHelp: PropTypes.bool,
+  closeHelp: PropTypes.func,
 }
 HelpView.defaultProps = {
-  currentTab: null,
+  //currentTab: null,
   defaultReadme: "dashboard",
+  popupMode: false,
+  showHelp: true,
+  closeHelp: () => { },
 }
