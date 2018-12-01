dfc85abbcc0fbb002b24704b7f70f7c7cb160be0
Sandi Chakravarty <sandi@fortmasongames.com>
2018-12-01 00:59:55 -0800

    fix for songlist regular sorting not working

--- src/sqliteService.js
+++ src/sqliteService.js
@@ -487,6 +487,7 @@ export async function getSongsOwned(start = 0, count = 10, sortField = "mastery"
     const dbfilename = window.sqlitePath;
     db = await window.sqlite.open(dbfilename);
   }
+  console.log(sortOptions)
   let sql;
   let searchSql = `( song like '%${escape(search)}%' or 
             artist like '%${escape(search)}%' or 
@@ -895,6 +896,7 @@ export async function getSongsFromGeneratedPlaylist(
 }
 export async function getSongsFromPlaylistDB(dbname, start = 0, count = 10, sortField = "mastery", sortOrder = "desc", search = "", searchField = "", options = [], sortOptions = []) {
   // console.log("__db_call__: getSongsFromPlaylistDB");
+  console.log(sortOptions);
   if (db == null) {
     const dbfilename = window.sqlitePath;
     db = await window.sqlite.open(dbfilename);
