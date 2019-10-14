2d5d5481ae01042a2afdba10b1fcc0e2f762b9a1
Sandi Chakravarty <sandi.newton@gmail.com>
2019-10-14 02:54:02 -0700

    update dl links based on releases/latest api

--- docs/js/main.js
+++ docs/js/main.js
@@ -198,3 +198,18 @@ function goTo(tag) {
         window.owl.trigger("play.owl.autoplay");
     }, 5000);
 }
+
+fetchLatestVersion = async () => {
+    const data = await fetch("https://api.github.com/repos/sandiz/rs-manager/releases/latest");
+    const json = await data.json();
+    const assets = json.assets;
+    $("#version").html(json.tag_name + " - " + json.name);
+    for (let i = 0; i < assets.length; i += 1) {
+        const asset = assets[i];
+        if (asset.name.endsWith(".dmg"))
+            $("#mac-dl").attr('href', asset.browser_download_url);
+        if (asset.name.endsWith(".exe"))
+            $("#win-dl").attr('href', asset.browser_download_url);
+    }
+}
+fetchLatestVersion();
